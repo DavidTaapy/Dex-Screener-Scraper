668f6165bdf5f70d839cdea3aea2d8dbb39672b8
DavidTaapy <74765863+DavidTaapy@users.noreply.github.com>
2021-12-23 16:01:39 +0800

    Got data and processed into dataframe

--- app.py
+++ app.py
@@ -8,8 +8,6 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
-tables = []
-
 # Initialize the URLs
 urls = [
     "https://dexscreener.com/new-pairs",                # First Page of New Pairs 
@@ -17,6 +15,43 @@ urls = [
     "https://dexscreener.com/new-pairs/page-3",         # Third Page of New Pairs
     "https://dexscreener.com/new-pairs/page-4"          # Fourth Page of New Pairs
 ]
+
+# Function to turn retrieved data into a dataframe
+def processData(data):
+    # Remove V2 Column
+    data = list(filter(lambda x: x != 'V2', data))
+    # Change here if first column is changed
+    firstColumn = "TOKEN"   
+    firstColumnIndex = data.index(firstColumn)
+    # First Data Element
+    if data.index("#1"):
+        firstDataIndex = data.index("#1")
+    elif data.index("#101"):
+        firstDataIndex = data.index("#101")
+    elif data.index("#201"):
+        firstDataIndex = data.index("#201")
+    elif data.index("#301"):
+        firstDataIndex = data.index("#301")
+    else:
+        return "Error in getting first data index!"
+    # Get number of columns by deducted index of first data element from index of first column element   
+    numColumns = firstDataIndex - firstColumnIndex + 2 # The two additional ones are the "#Num" & "Token Pair"
+    # Find the number of rows
+    numRows = int((len(data) - firstDataIndex) / numColumns)
+    # Get the column names
+    columns = ["Number", "Currency Pair"]
+    columns += data[firstColumnIndex : firstColumnIndex + numColumns - 2] # Minus 2 as we already have the first two columns in the columns list
+    # Initialise new dataframe
+    df = pd.DataFrame(columns = columns)
+    df_length = 0
+    # Add the data into the dataframe
+    for _ in range(numRows):
+        rowElements = data[firstDataIndex : firstDataIndex + numColumns]
+        df.loc[df_length] = rowElements # Add data elements as a row to dataframe
+        firstDataIndex += numColumns # Increment index to become the first element of the next data row
+        df_length += 1 # Increment number of rows in dataframe
+    
+    return df
 
 # Initialize Driver
 driver = webdriver.Firefox()
@@ -29,7 +64,10 @@ for url in urls:
             EC.presence_of_element_located((By.CLASS_NAME, 'css-18z8t27'))
         )
         if data:
-            print(data.text.split('\n'))
+            data = data.text.split('\n')
+            tempDf = processData(data)
+            print(tempDf)
+        break
         time.sleep(1) # Delay by one second
     except Exception as e:
         print(e)
