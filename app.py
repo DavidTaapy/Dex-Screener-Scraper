a0f64e2bd5f83d4ca3a5f217a52db5113edaf548
DavidTaapy <DavidTayAP@Hotmail.Com>
2021-12-25 00:48:21 +0800

    Dynamic Scraping Additions
    Automatically finds the number of pages and scrapes all given a base address

--- app.py
+++ app.py
@@ -5,32 +5,19 @@ import time
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-
-# Initialize the URLs
-urls = [
-    "https://dexscreener.com/new-pairs",                # First Page of New Pairs 
-    "https://dexscreener.com/new-pairs/page-2",         # Second Page of New Pairs
-    "https://dexscreener.com/new-pairs/page-3",         # Third Page of New Pairs
-    "https://dexscreener.com/new-pairs/page-4"          # Fourth Page of New Pairs
-]
+from selenium.webdriver.support import expected_conditions as EC        
 
 # Function to turn retrieved data into a dataframe
-def processData(data):
+def processData(data, firstIndex):
     # Remove V2 Column
     data = list(filter(lambda x: x not in ['V1', 'V2', 'V3'], data))
     # Change here if first column is changed
     firstColumn = "TOKEN"   
     firstColumnIndex = data.index(firstColumn)
     # First Data Element
-    if "#1" in data:
-        firstDataIndex = data.index("#1")
-    elif "#101" in data:
-        firstDataIndex = data.index("#101")
-    elif "#201" in data:
-        firstDataIndex = data.index("#201")
-    elif "#301" in data:
-        firstDataIndex = data.index("#301")
+    indexString = "#" + str(firstIndex)
+    if indexString in data:
+        firstDataIndex = data.index(indexString)
     else:
         return "Error in getting first data index!"
     # Get number of columns by deducted index of first data element from index of first column element   
@@ -47,7 +34,7 @@ def processData(data):
     while firstDataIndex < len(data):
         rowElements = data[firstDataIndex : firstDataIndex + numColumns]
         for element in rowElements:
-            if '$' in element and '.' in element:               # Find Price
+            if ('$' in element and '.' in element) or element == '$0':  # Find Price
                 priceIndex = rowElements.index(element)
                 break
         finalRowElements = rowElements[0 : priceIndex]
@@ -58,37 +45,87 @@ def processData(data):
             tempNumColumns -= 1
             firstDataIndex -= 1
         finalRowElements += rowElements[priceIndex : tempNumColumns]
+        # print(finalRowElements)
         df.loc[df_length] = finalRowElements # Add data elements as a row to dataframe
         firstDataIndex += numColumns # Increment index to become the first element of the next data row
         df_length += 1 # Increment number of rows in dataframe
     
     return df
 
-# Initialize Driver
-driver = webdriver.Firefox()
-# Initialize Empty Dataframe
-masterDf = pd.DataFrame()
-# Scrape each url page
-for url in urls:
-    # Statement to inform about progress
-    print('Scraping {0}'.format(url))
-    try:
-        driver.get(url)
-        data = WebDriverWait(driver, 10).until(
-            EC.presence_of_element_located((By.CLASS_NAME, 'css-18z8t27'))
-        )
-        if data:
-            data = data.text.split('\n')
-            tempDf = processData(data)
-            print(tempDf)
-            masterDf = masterDf.append(tempDf)
-        time.sleep(1) # Delay by one second
-    except Exception as e:
-        print(e)
-        continue
+# # Process Data After Retrieval
+# def postProcess(df):
+#     # Dictionary for units
+#     units = {
+#         'K': 10 ** 3,
+#         'M': 10 ** 6,
+#         'B': 10 ** 9,
+#         'T': 10 ** 12
+#     }
 
-# Close Driver
-driver.close()
+#     # Remove tokens exhibiting ridiculous market capitalizations
+#     df = df.loc[(df['MKT CAP'] != '>$999T') & (df['MKT CAP'] != '<$1') & (df['MKT CAP'] != '-')]
+#     # Convert Market Cap to Integer
+#     # df['MKT CAP'] = df['MKT CAP'].apply(lambda x: float(x[1:-1] * units[x[-1]]))
+#     # Remove entries with Market Cap less than 2 Million
+#     # twoMillion = 2 * (10 ** 6)
+#     # df = df.loc[df['MKT CAP'] >= twoMillion]
+#     # Sort Market Cap in descending order
+#     # df = df.sort_values(by = ['MKT CAP'], ascending = False)
 
-# Export the table
-masterDf.to_csv("Data.csv", index = False)
+#     return df
+
+# Scrape Dex Screener For Data
+def scrapeDex():
+    # Initialize the URLs - Add the first pages of the genre to scrape
+    urls = [   
+        "https://dexscreener.com/new-pairs"             # First Page of New Pairs
+    ]        
+    # Initialize Driver
+    driver = webdriver.Firefox()
+    # Initialize Empty Dataframe
+    masterDf = pd.DataFrame()
+    # Set first index of current page
+    firstIndex = 1
+   
+    # Scrape each url page
+    for url in urls:       
+        while True:
+            # Statement to inform about progress
+            print('Scraping with first index = {0}'.format(firstIndex))
+            try:
+                driver.get(url)
+                data = WebDriverWait(driver, 10).until(
+                    EC.presence_of_element_located((By.CLASS_NAME, 'css-18z8t27'))
+                )
+                nextData = WebDriverWait(driver, 10).until(
+                    EC.presence_of_element_located((By.CLASS_NAME, 'css-v0p5eh'))
+                )
+                if data:
+                    data = data.text.split('\n')
+                    tempDf = processData(data, firstIndex)
+                    masterDf = masterDf.append(tempDf)
+                    firstIndex += 100
+                    if str(firstIndex) not in nextData.text:
+                        break
+                    else:
+                        # Change url to include new page number
+                        if "page" not in url:   # If first page
+                            url += "/page-2"
+                        else:
+                            baseURL, pageNum = url.split("page-")   # If a page number is already present
+                            url = baseURL + "page-" + str(int(pageNum) + 1)
+                time.sleep(1) # Delay by one second
+            except Exception as e:
+                print(e)
+                continue
+
+    # Close Driver
+    driver.close()
+    # Post Process Dataframe
+    masterDf = postProcess(masterDf)
+    # Export the table
+    masterDf.to_csv("Data.csv", index = False)
+
+# Run the following when program is executed
+if __name__ == "__main__":
+    scrapeDex()
